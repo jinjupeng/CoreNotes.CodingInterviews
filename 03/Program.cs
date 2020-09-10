8ace740fa556ce75efed347bc789da5a1cf0c7c6
jinjupeng <im.jp@outlook.com>
2020-09-10 10:35:13 +0800

    添加项目文件。

---
+++ 03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// 剑指 Offer 03. 数组中重复的数字
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int FindRepeatNumber(int[] nums)
+        {
+
+        }
+    }
+}
