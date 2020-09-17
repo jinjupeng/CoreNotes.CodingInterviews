3e0515b13f8a4ac78b3a46c06ba386a3f5d00e77
jinjupeng <im.jp@outlook.com>
2020-09-17 11:09:08 +0800

    剑指 Offer 06. 从尾到头打印链表

---
+++ 06/ListNode.cs
@@ -0,0 +1,9 @@
+﻿namespace _06
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+}
---
+++ 06/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var head = new ListNode(1) {next = new ListNode(3) {next = new ListNode(2)}};
+            var pro = new Program();
+            var data = pro.ReversePrint(head);
+        }
+
+        /// <summary>
+        /// 剑指 Offer 06. 从尾到头打印链表
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public int[] ReversePrint(ListNode head)
+        {
+            if (head == null)
+            {
+                return new int[]{};
+            }
+            // 思路：使用栈
+            var nodeStack = new Stack<int>();
+            // 入栈
+            while (head != null)
+            {
+                nodeStack.Push(head.val);
+                head = head.next;
+            }
+
+            var array = new int[nodeStack.Count];
+            for (int i = 0; i < array.Length; i++)
+            {
+                // 出栈
+                array[i] = nodeStack.Pop();
+            }
+
+            return array;
+        }
+    }
+}
