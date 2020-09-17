ef2ca94b69a751118d2e4fe19383696e016b000d
jinjupeng <im.jp@outlook.com>
2020-09-17 09:23:56 +0800

    剑指 Offer 55 - I. 二叉树的深度

---
+++ 55-1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace _55_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new Tree();
+            var treeNode = new TreeNode(3)
+            {
+                left = new TreeNode(9), right = new TreeNode(20) {left = new TreeNode(15), right = new TreeNode(7)}
+            };
+            var data = tree.MaxDepth(treeNode);
+        }
+    }
+}
---
+++ 55-1/Tree.cs
@@ -0,0 +1,39 @@
+﻿namespace _55_1
+{
+    public class Tree
+    {
+        /// <summary>
+        /// 剑指 Offer 55 - I. 二叉树的深度
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            // 思路：深度优先遍历搜索(就是递归)
+            var leftDepth = MaxDepth(root.left);
+            var rightDepth = MaxDepth(root.right);
+
+            if (leftDepth > rightDepth)
+            {
+                return leftDepth + 1;
+            }
+
+            return rightDepth + 1;
+        }
+    }
+
+    /// <summary>
+    /// 树节点
+    /// </summary>
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+}
