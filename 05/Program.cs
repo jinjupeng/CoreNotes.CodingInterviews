e9f6181b7ad23d56fab9c1e5117f1d2cabf3529a
jinjupeng <im.jp@outlook.com>
2020-09-17 16:30:51 +0800

    剑指 Offer 05. 替换空格

---
+++ 05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        /// <summary>
+        /// 剑指 Offer 05. 替换空格
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public string ReplaceSpace(string s)
+        {
+            return s.Replace(" ", "%20");
+        }
+    }
+}
