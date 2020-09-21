4c0c5cfc87977645671dac9e9129331148172149
jinjupeng <im.jp@outlook.com>
2020-09-21 09:14:14 +0800

    剑指 Offer 32 - II. 从上到下打印二叉树 II

---
+++ 33-2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _33_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new Tree();
+            var treeNode = new TreeNode(3)
+            {
+                left = new TreeNode(9),
+                right = new TreeNode(20) { left = new TreeNode(15), right = new TreeNode(7) }
+            };
+            var data = tree.LevelOrder(treeNode);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 33-2/Tree.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace _33_2
+{
+    public class Tree
+    {
+        /// <summary>
+        /// 剑指 Offer 32 - II. 从上到下打印二叉树 II
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            // 思路：使用队列
+            var list = new List<IList<int>>();
+            if (root == null)
+            {
+                return list;
+            }
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count > 0)
+            {
+                var size = queue.Count;
+                var level = new List<int>();
+                for (int i = 0; i < size; i++)
+                {
+                    var outQueue = queue.Dequeue();
+                    level.Add(outQueue.val);
+                    if (outQueue.left != null)
+                    {
+                        queue.Enqueue(outQueue.left);
+                    }
+
+                    if (outQueue.right != null)
+                    {
+                        queue.Enqueue(outQueue.right);
+                    }
+
+                }
+                list.Add(level);
+            }
+
+            return list;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+}
