a5387060bda758060bd2ce3ed3aa0c1fe56d86de
猫先生 <im.jp@outlook.com>
2022-04-07 12:17:56 +0800

    update

---
+++ 02_Singleton/Program.cs
@@ -0,0 +1,169 @@
+﻿// 面试题2：实现Singleton模式
+// 题目：设计一个类，我们只能生成该类地一个实例
+
+using System;
+
+namespace _02_Singleton
+{
+    /// <summary>
+    /// 不好的解法一：只适用于单线程环境
+    /// </summary>
+    public sealed class Singleton1
+    {
+        /// <summary>
+        /// 构造函数设为私有函数以禁止他人创建实例
+        /// </summary>
+        private Singleton1()
+        {
+
+        }
+
+        private static Singleton1 instance = null;
+        public static Singleton1 Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new Singleton1();
+                }
+
+                return instance;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 不好地解法二：虽然在多线程环境中能工作，但是效率不高
+    /// </summary>
+    public sealed class Singleton2
+    {
+        private Singleton2()
+        {
+
+        }
+
+        private static readonly object syncObj = new object();
+
+        private static Singleton2 instance = null;
+        public static Singleton2 Instance
+        {
+            get
+            {
+                // 加锁比较耗时
+                lock (syncObj)
+                {
+                    if(instance == null)
+                    {
+                        instance = new Singleton2();
+                    }
+
+                    return instance;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 可行地解法：加同步锁前后两次判断实例是否已存在
+    /// 在实例还没有创建之前需要加锁操作，以保证只有一个线程创建出实例。
+    /// 而当实例已经创建之后，已经不需要在执行加锁操作了。
+    /// </summary>
+    public sealed class Singleton3
+    {
+        private Singleton3()
+        {
+        }
+
+        private static object syncObj = new object();
+
+        private static Singleton3 instance = null;
+        public static Singleton3 Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncObj)
+                    {
+                        if (instance == null)
+                            instance = new Singleton3();
+                    }
+                }
+
+                return instance;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 强烈推荐的解法一：利用静态构造函数
+    /// C#语法中有一个函数能够确保只调用一次，那就是静态构造函数，可以利用这个特性实现单例模式。
+    /// </summary>
+    public sealed class Singleton4
+    {
+        private Singleton4()
+        {
+            Console.WriteLine("An instance of Singleton4 is created.");
+        }
+
+        public static void Print()
+        {
+            Console.WriteLine("Singleton4 Print");
+        }
+
+        private static Singleton4 instance = new Singleton4();
+        public static Singleton4 Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 强烈推荐的解法二：实现按需创建实例
+    /// </summary>
+    public sealed class Singleton5
+    {
+        Singleton5()
+        {
+            Console.WriteLine("An instance of Singleton5 is created.");
+        }
+
+        public static void Print()
+        {
+            Console.WriteLine("Singleton5 Print");
+        }
+
+        public static Singleton5 Instance
+        {
+            get
+            {
+                return Nested.instance;
+            }
+        }
+
+        class Nested
+        {
+            static Nested()
+            {
+            }
+
+            internal static readonly Singleton5 instance = new Singleton5();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 也会打印An instance of Singleton4 is created.
+            Singleton4.Print();
+
+            // 不会打印An instance of Singleton5 is created.
+            Singleton5.Print();
+        }
+    }
+}
---
+++ 03_02_DuplicationArrayNoEdit/Program.cs
@@ -0,0 +1,85 @@
+﻿// 面试题3（二）：不修改数组找出重复的数字
+// 题目：在一个长度为n+1的数组里的所有数字都在1到n的范围内，所以数组中至
+// 少有一个数字是重复的。请找出数组中任意一个重复的数字，但不能修改输入的
+// 数组。例如，如果输入长度为8的数组{2, 3, 5, 4, 3, 2, 6, 7}，那么对应的
+// 输出是重复的数字2或者3。
+
+using System;
+
+namespace _03_02_DuplicationArrayNoEdit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[] { 2, 3, 5, 4, 3, 2, 6, 7 };
+            var getDuplication = new Program().GetDuplication(array);
+            Console.WriteLine($"重复值为：{getDuplication}");
+        }
+
+        /// <summary>
+        /// 数组元素的取值范围是 [1, n]，对该范围对半划分，分成 [1, middle], [middle+1, n]。
+        /// 计算数组中有多少个(count)元素落在 [1, middle] 区间内，如果 count 大于 middle-1+1，那么说明这个范围内有重复元素，否则在另一个范围内。
+        /// 继续对这个范围对半划分，继续统计区间内元素数量。
+        /// 时间复杂度 O(n* log n)，空间复杂度 O(1)。
+        /// 注意，此方法无法找出所有重复的元素。
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public int GetDuplication(int[] numbers)
+        {
+            if (numbers == null || numbers.Length < 1)
+            {
+                return -1;
+            }
+
+            int start = 1;
+            int end = numbers.Length - 1;
+            while (end >= start)
+            {
+                int middle = start + ((end - start) >> 1);
+
+                // 调用 log n 次
+                int count = CountRange(numbers, start, middle);
+                if (start == end)
+                {
+                    if (count > 1)
+                    {
+                        return start;
+                    }
+                    break;
+                }
+                else
+                {
+                    // 无法找出所有重复的数
+                    if (count > (middle - start) + 1)
+                    {
+                        end = middle;
+                    }
+                    else
+                    {
+                        start = middle + 1;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        private int CountRange(int[] numbers, int start, int end)
+        {
+            if (numbers == null)
+            {
+                return 0;
+            }
+            int count = 0;
+            foreach (int e in numbers)
+            {
+                if (e >= start && e <= end)
+                {
+                    ++count;
+                }
+            }
+            return count;
+        }
+    }
+}
